54a2c9def7c4b623d1059f09b75ebad0343b3e83
Nicolas Brignol <tools@octet-studio.com>
2016-04-03 16:33:25 +0200

    Nettoyage

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 
 //scene en cours, avec la valeur de depart.
-var currentSceneId = 1;
+var currentSceneId = 131;
 
 //preparation de jxServer
 var jxServer = new JX.Server();
 
 //initialisation des variables du jeu. 
-jxServer.variables.init("inspiration", 0);
+jxServer.variables.init("finA", 0);
+jxServer.variables.init("finB", 0);
+jxServer.variables.init("finC", 0);
+
 
 //cette fonction sera appellee quand le JSON de la scene sera recu.
 var handleScene = function(jsonData){
@@ -20,22 +23,16 @@ var handleScene = function(jsonData){
 	var titleElement = document.getElementById("mainTitle");
 	var mediaElement = document.getElementById("medias");
 	var connectionElement = document.getElementById("connections");
-	var playerElement = document.getElementById("player");
-	var inputElement = document.getElementById("playerInput");
 	
 	//reset elements
 	titleElement.innerHTML = "";
 	mediaElement.innerHTML = "";
 	connectionElement.innerHTML = "";
-	playerElement.innerHTML = "";
-	inputElement.value="";
 	
 	//prise en compte des actions (mise a jour des variables de la scène, s'il y en a)
 	jxServer.variables.update(jsonData.actions);
 
-	//affichage
-	playerElement.innerHTML = "Votre score d'inspiration : " + jxServer.variables.get("inspiration");
-
+	
 	//titre de la page
 	titleElement.innerHTML = "Scène #" + jsonData.id ;
 	titleElement.innerHTML += " : " + jsonData.title ;
@@ -46,10 +43,23 @@ var handleScene = function(jsonData){
 			mediaElement.innerHTML += "<p>" + item.content + "</p>";	
 		} 
 
-		if (item.format == "image") {
+		if (item.format == "png") {
 			var newImageElement = document.createElement("img");
 			newImageElement.setAttribute("src", item.content);
 			mediaElement.appendChild(newImageElement);
+		}
+
+		if (item.format == "mp3") {
+			var audioElement = document.createElement("audio");
+			//audioElement.setAttribute("controls", "controls");
+			audioElement.setAttribute("autoplay", "autoplay");
+			
+			var sourceElement = document.createElement("source");
+			sourceElement.setAttribute("type", "audio/mpeg");
+			sourceElement.setAttribute("src", item.content);
+
+			audioElement.appendChild(sourceElement);
+			mediaElement.appendChild(audioElement);
 		}
 	});
 
@@ -60,8 +70,7 @@ var handleScene = function(jsonData){
 		
 		var newConnectionElement = document.createElement("li");
 		newConnectionElement.innerHTML = item.label;
-		newConnectionElement.innerHTML += " (vers la scène #" + item.childSceneId + ")";
-
+		
 		newConnectionElement.addEventListener("click", function(){
 			jxServer.requestScene(item.childSceneId, handleScene);
 		});
@@ -70,28 +79,6 @@ var handleScene = function(jsonData){
 	});
 
 }
-
-//cette fonction sera appelle si la saisie du joueur correspond a une connection.
-var handlePatternResponse = function(jsonData){
-	console.log("Check pattern : scene found !");
-	handleScene(jsonData);
-}
-
-//cette fonction sera appelle si la saisie du joueur NE correspond PAS a une connection.
-var handlePatternFailure = function(message, data){
-	console.log("Check pattern : no scene found.");
-	//alert("Invalid pattern");
-}
-
-//gestion de la validation du formulaire
-document.querySelector("#patternForm").addEventListener("submit", function(){
-
-	var theInput = document.querySelector("#playerInput").value;
-	console.log("Form submitted with value : " + theInput);
-	
-	//demande au serveur la scene correspondant a la saisie (en passant les fonctions "handlePatternResponse" et "handlePatternFailure")
-	jxServer.checkPattern(currentSceneId, theInput, handlePatternResponse, handlePatternFailure);	
-});
 
 //******
 //c'est ici que ca demarre : lance la requete pour la premiere scene.
