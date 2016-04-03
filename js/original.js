dfda9cebe41e72b9161326b11b2a489c81cd0e11
Annabel Roux <annabel@fbrq.fr>
2016-04-03 13:57:50 +0200

    Updated generic template

--- js/original.js
+++ js/original.js
@@ -6,8 +6,11 @@ debugger;
 var jxServer = window.jxServer = new JX.Server();
 
 //scene en cours, avec la valeur de depart.
-var currentSceneId = parseInt(sessionStorage.getItem('JX_lastSeenScene'), 10) || 79;
+//var currentSceneId = parseInt(sessionStorage.getItem('JX_lastSeenScene'), 10) || 9;
+    
+    var currentSceneId = 123;
 
+var fonctionFullscreen;
 
 //initialisation des variables du jeu. 
 jxServer.variables.init("geoloc", "off");
@@ -15,17 +18,25 @@ jxServer.variables.init("beenthere", 0);
 jxServer.variables.init("vue", 0);
 jxServer.variables.init("indiceVue", 0);
 jxServer.variables.init("lastSeenScene", 9);
+    
+var mainContainer=document.getElementById("container");
 
 
 //cette fonction sera appellee quand le JSON de la scene sera recu.
 var handleScene = function(jsonData){
+    
+    if (fonctionFullscreen) {
+
+        mainContainer.removeEventListener("click", fonctionFullscreen);
+        fonctionFullscreen = undefined;
+
+    }
 
     console.log("handleScene receive JSON data : ");
     console.log(jsonData);
 
     currentSceneId = jsonData.id;
-    debugger;
-    jxServer.variables.update("lastSeenScene", jsonData.id);
+    sessionStorage.setItem("JX_lastSeenScene", jsonData.id);
 
     //pointe vers les elements HTML
     var mediaElement = document.getElementById("medias");
@@ -58,6 +69,22 @@ var handleScene = function(jsonData){
             newImageElement.setAttribute("width", "200");
             mediaElement.appendChild(newImageElement);
         }
+        
+        if (item.format == "video") {
+            var newVideoElement = document.createElement("iframe");
+            newVideoElement.setAttribute("src", item.content);
+            newVideoElement.setAttribute("width", "500");
+            newVideoElement.setAttribute("height", "406");
+            mediaElement.appendChild(newVideoElement);
+        }
+        
+        if (item.format == "audio") {
+            var newVideoElement = document.createElement("iframe");
+            newAudioElement.setAttribute("src", item.content);
+            newAudioElement.setAttribute("width", "80%");
+            newAudioElement.setAttribute("height", "auto");
+            mediaElement.appendChild(newAudioElement);
+        }
     });
 
     jsonData.connections.forEach(function(item){
@@ -65,31 +92,75 @@ var handleScene = function(jsonData){
             return;
         }
                 
+        if (item.position == "100")
+
+        {
+
+            fonctionFullscreen = function(){
+                jxServer.requestScene(item.childSceneId, handleScene);
+            };
+
+            mainContainer.addEventListener("click", fonctionFullscreen);
+
+        }
+
+        else {
+
             if (item.position == "105") {
 
                 var newConnectionElement = document.createElement("i");
-                newConnectionElement.setAttribute("class", "fa fa-chevron fa-4x");
+                newConnectionElement.setAttribute("class", "fa fa-circle fa-4x");
                 newConnectionElement.addEventListener("click", function(){
+                    newConnectionElement.setAttribute("class", "active");
                     jxServer.requestScene(item.childSceneId, handleScene);
                 });
 
                 connectionElement.appendChild(newConnectionElement);
             }
             else {
-            var newConnectionElement = document.createElement("button");
-            newConnectionElement.innerHTML = item.label;
-            newConnectionElement.setAttribute("class", "pure-button pure-button-primary");
-            newConnectionElement.setAttribute("style", "margin-right:20px;");
-            newConnectionElement.addEventListener("click", function(){
-                jxServer.requestScene(item.childSceneId, handleScene);
-                if (item.childSceneId == 10) {
-                    var geoloc = "on";
+                
+                if (item.label == "next") {
+                    
+                    var newConnectionElement = document.createElement("i");
+                    newConnectionElement.setAttribute("class", "fa fa-chevron-right fa-2x");
+                    newConnectionElement.addEventListener("click", function(){
+                        jxServer.requestScene(item.childSceneId, handleScene);
+                    });
+
+                    connectionElement.appendChild(newConnectionElement);
                 
                 }
-            });
+                
+                else {
+                    
+                    
+                    if (item.position == "200") {
 
-            connectionElement.appendChild(newConnectionElement);
+                        var newConnectionElement = document.createElement("img");
+                        newConnectionElement.setAttribute("src", item.label);                                                  
+                        newConnectionElement.setAttribute("class", "img-round");
+                        newConnectionElement.addEventListener("click", function(){
+                            jxServer.requestScene(item.childSceneId, handleScene);
+                        });
+
+                        connectionElement.appendChild(newConnectionElement);
+
+                    }
+                    else {
+                        var newConnectionElement = document.createElement("button");
+                        newConnectionElement.innerHTML = item.label;
+                        newConnectionElement.setAttribute("class", "pure-button pure-button-primary");
+                        newConnectionElement.setAttribute("style", "margin-right:20px;");
+                        newConnectionElement.addEventListener("click", function(){
+                            jxServer.requestScene(item.childSceneId, handleScene);
+                        });
+
+                        connectionElement.appendChild(newConnectionElement);
+                    }
+                    
+                }
             }
+        }
     });
 
 }
